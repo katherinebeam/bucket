ca2fe454ad0b2a049f9b7211be01d9006411b82c
Katherine Beam <k.beam@umiami.edu>
2015-11-08 16:16:41 -0500

    Add validations to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108145024) do
+ActiveRecord::Schema.define(version: 20151108210112) do
 
   create_table "bucket_lists", force: :cascade do |t|
     t.string   "title"
@@ -29,5 +29,19 @@ ActiveRecord::Schema.define(version: 20151108145024) do
   end
 
   add_index "bucketlist_items", ["bucket_list_id"], name: "index_bucketlist_items_on_bucket_list_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "picture"
+    t.text     "bio"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end
