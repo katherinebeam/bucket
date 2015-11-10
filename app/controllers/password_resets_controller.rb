a1d51dc9a56626eab97c2c6886848586e63034c2
Katherine Beam <k.beam@umiami.edu>
2015-11-09 19:02:40 -0500

    Created edit password reset view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,7 +7,7 @@ class PasswordResetsController < ApplicationController
     if user
       user.generate_password_reset_token!
       Notifier.password_reset(user).deliver
-      flash[:success] = "Check your email! We've sent a link to reset your password"
+      flash[:success] = "Check your email! We've sent a link to reset your password."
       redirect_to login_path
     else
       flash.now[:notice] = "Email not found."
@@ -15,10 +15,13 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
   def update
     user = User.find_by(email: params[:email])
     user.generate_password_reset_token!
-    flash[:success] = "Check your email! We've sent a link to reset your password"
+    flash[:success] = "Check your email! We've sent a link to reset your password."
     redirect_to login_path
   end
 end
